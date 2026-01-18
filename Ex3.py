fef0c9ede6beb407c42f8e83b878bae7d2ef5206
HuyHaLong <huyarsenal0707@gmail.com>
2026-01-18 11:45:39 +0700

    ex3 python exercise

---
+++ Ex3.py
@@ -0,0 +1,6 @@
+length= float(input("Enter the length of the rectangle: "))
+width= float(input("Enter the width of the rectangle: "))
+perimeter= 2 * (length + width)
+area= length * width
+print("Perimeter of the rectangle is:", perimeter)
+print("Area of the rectangle is:", area)
