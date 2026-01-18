878f26f2bbf5fcaf3deb451da5d75f048a3ba3fd
HuyHaLong <huyarsenal0707@gmail.com>
2026-01-18 11:54:33 +0700

    ex4 python exercise

---
+++ Ex4.py
@@ -0,0 +1,9 @@
+x= int(input("Enter first number: "))
+y= int(input("Enter second number: "))
+z= int(input("Enter third number: "))
+sum= x + y + z
+product= x * y * z
+average= sum / 3
+print("Sum is:", sum)
+print("Product is:", product)
+print("Average is:", average)
